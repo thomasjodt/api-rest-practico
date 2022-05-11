cd52dd2ee45282d828639f0e1b8ecbdcf0b63d15
thomasjodt <jodt5200@gmail.com>
2022-05-11 11:59:28 -0500

    Added the functions for get preview movie images and categories, also added the function for loading the diferent functions while hash change

---
+++ src/navigation.js
@@ -0,0 +1,31 @@
+window.addEventListener('load', navigator, false)
+window.addEventListener('hashchange', navigator, false)
+
+function navigator() {
+  // console.log(location)
+
+  if (location.hash.startsWith('#trends'))          { trendsPage() }
+  else if (location.hash.startsWith('#search='))    { searchPage() }
+  else if (location.hash.startsWith('#movie='))     { movieDetailsPage() }
+  else if (location.hash.startsWith('#category='))  { categoriesPage() }
+  else { homePage() }
+}
+
+function homePage() {
+  console.log('Home!!')
+  
+  getTrendingMoviesPreview()
+  getCategoriesPreview()
+}
+function categoriesPage() {
+  console.log('Categories!!!')
+}
+function trendsPage() {
+  console.log('Trends!!')
+}
+function searchPage() {
+  console.log('Search!!!')
+}
+function movieDetailsPage() {
+  console.log('Movie Details!!!')
+}
